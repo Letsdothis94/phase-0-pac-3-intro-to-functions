ff270fc631e9ab63a66e40deaa25349757f83a7d
Cristian Contreras <contreras.cristian994@gmail.com>
2022-10-08 07:39:39 -0400

    commit tree 5c2d3801e56e6642c944e57139c19adaac378a84

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Follow along with the examples here
+function doNothing() {}
+function sayHello() {
+    console.log("Hello!");
+}
+sayHello();
+
+function sayHelloToGuadalupe() {
+    console.log("Hello, Guadalupe!");
+}
+sayHelloToGuadalupe();
+
+function sayHelloToLiz() {
+    console.log("Hello, Liz!");
+}
+sayHelloToLiz();
+
+function sayHelloToSamip() {
+    console.log("Hello, Samip!");
+}
+sayHelloToSamip();
+
+sayHelloToGuadalupe();
+sayHelloToLiz();
+sayHelloToSamip();
+
+
+function say("Goodbye", "Julio");
